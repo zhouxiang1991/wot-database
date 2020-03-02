3c9cbac1e3cc23f87c9de32dc3679a2c447a5c68
zhouxiang <zhouxiang219@163.com>
2020-03-03 00:16:29 +0800

    html

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+  },
+  extends: ["standard"],
+  parserOptions: {
+   ecmaVersion: 6
+  }
+}
--- index.js
+++ index.js
@@ -1,29 +1,90 @@
+const fs =require('fs')
+const path =require('path')
 const Mongo = require('./mongo')
 const mongoose = require('mongoose')
 const puppeteer = require('puppeteer')
 const tankSchema = require('./schema/tank')
-// const tank = new Mongo('mongodb://172.0.0.1:27017/wot', 'tanks', tankSchema)
-mongoose.connect('mongodb://localhost:27017/wot', {
-  useNewUrlParser: true, useUnifiedTopology: true
-})
-mongoose.connection.on('connected', function () { 
+const dbURL = 'mongodb://localhost:27017/wot'
+const tank = new Mongo(dbURL, 'tanks', tankSchema)
+const tankLinkList = require('./tankLinkList.json')
+// const nationLinkList = require('./nationLinkList.json')
+// console.log(tankLinkList)
 
-console.log('Mongoose connection open to ' + DB_URL); }); /** * 连接异常 */ 
+// const data = fs.readFileSync(path.resolve('./data.json'))
+// const nationLinkList = fs.readFileSync(path.resolve('./nationLinkList.json'))
+// const tankLinkList = fs.readFileSync(path.resolve('./tankLinkList.json'))
 
-mongoose.connection.on('error',function (err) { 
+// const URL = 'https://wiki.wargaming.net/en/World_of_Tanks'
+// const URL = 'https://www.baidu.com'
 
-console.log('Mongoose connection error: ' + err); }); /** * 连接断开 */ 
-
-mongoose.connection.on('disconnected', function () { 
-
-console.log('Mongoose connection disconnected'); });
+const URL = 'http://www.xiaosj.cn/tx/tank/list.aspx'
 
 async function start () {
-  const browser = await puppeteer.launch(); 
+  const browser = await puppeteer.launch({
+    // headless: false
+  }); 
+
   const page = await browser.newPage(); 
-  await page.goto('http://www.baidu.com'); 
-  const s = await page.waitForXPath('//*[@id="u_sp"]/a[4]')
-  console.log(s)
+  await page.goto(URL); 
+  const nationContainer = 'body > div:nth-child(2)'
+  const nationLink = nationContainer + ' > a:not(:first-child)'
+  await page.waitForSelector(nationContainer)
+  const nationLinkList = await page.$$eval(nationLink, e => Array.from(e).filter(item => item.innerText !== '首页').map(item => item.href))
+  fs.writeFileSync(path.resolve('./nationLinkList'), JSON.stringify(nationLinkList),{encoding: 'utf-8'})
+  const tankLinkList = []
+
+  for (let index = 0; index < nationLinkList.length; index++) {
+    const nationLink = nationLinkList[index];
+    // const type = await nations[0].evaluate(e => e.innerText.slice(0, 1))
+    const nationCode = nationLink.slice(-1)
+    // await nation.click()
+    // console.log(nationCode)
+    // nations[1].click()
+    await page.goto(nationLink)
+    // await page.waitFor(3000)
+    const containerSelector = '#content > #' + nationCode
+    // console.log(typeContainerSelector)
+    await page.waitForSelector(containerSelector)
+    const tankSelector = containerSelector + ' > div.panel-body > ul > .tanktypebox > ul > li > a'
+    const urls = await page.$$eval(tankSelector, e => Array.from(e).map(e => e.href))
+    tankLinkList.push(...urls)
+    // console.log(tankLinkList)
+    await page.waitFor(3000)
+    // if (nationCode === 'D') {
+    //   process.exit()
+    // }
+  }
+  fs.writeFileSync(path.resolve('./tankLinkList'), JSON.stringify(tankLinkList),{encoding: 'utf-8'})
+
+  for (let index = 0; index < tankLinkList.length; index++) {
+    const link = tankLinkList[index];
+  }
+
+  // console.log(tankLinkList.length)
   process.exit()
 }
 // start()
+
+async function traverse() {
+  const browser = await puppeteer.launch({
+    // headless: false
+  }); 
+
+  const page = await browser.newPage(); 
+
+  for (let index = 0; index < tankLinkList.length; index++) {
+    const tankLink = tankLinkList[index];
+    console.log(tankLink)
+    const id = tankLink.slice(tankLink.lastIndexOf('id=')).replace('id=', '')
+    await page.goto(tankLink)
+    await page.waitForSelector('#tankinfotable')
+    const html = await page.$eval('#tankinfotable', e => e.innerHTML)
+    // console.log(html)
+    fs.writeFileSync(path.resolve('./html/'+id+'.html'), html, {encoding:'utf-8'})
+    await page.waitFor(2000)
+    // process.exit()
+  }
+  process.exit()
+}
+
+traverse()
